1f2a1f8b4c182f5230fd4cee43ead540bbff3cdc
Max Malecki <mmalecki@oroinc.com>
2016-09-13 08:29:32 +0200

    Step provider

---
+++ src/App/IssueBundle/Model/Provider/StatusesProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\IssueBundle\Model\Service\Provider;
+
+use Doctrine\ORM\EntityManager;
+
+class StatusesProvider
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * StatusesProvider constructor.
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStatusesArray()
+    {
+        $result = [];
+
+        $steps = $this->entityManager->getRepository('Oro\Bundle\WorkflowBundle\Entity\WorkflowStep')
+            ->createQueryBuilder('s')
+            ->orderBy('s.label', 'asc')
+            ->getQuery()
+            ->getScalarResult();
+
+        foreach ($steps as $step) {
+            $result[$step['s_id']] = $step['s_label'];
+        }
+
+        return $result;
+    }
+}
