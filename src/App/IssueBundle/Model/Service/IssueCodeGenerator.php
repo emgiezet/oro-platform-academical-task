4015db2ecbdc2482196f0ee5b11d2ebed61d27f0
Max Malecki <mmalecki@oroinc.com>
2016-09-13 08:30:00 +0200

    issue code generator

---
+++ src/App/IssueBundle/Model/Service/IssueCodeGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace IssuesBundle\Model\Service;
+
+use Doctrine\ORM\EntityManager;
+use App\IssueBundle\Entity\Issue;
+
+/**
+ * Class IssueCodeGenerator
+ * @package IssuesBundle\Model\Service
+ */
+class IssueCodeGenerator
+{
+    /**
+     * @param EntityManager $entityManager
+     * @param Issue $issue
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function populateCode(EntityManager $entityManager, Issue $issue)
+    {
+        $today = new \DateTime('now');
+        $today->setTime(0, 0, 0);
+
+        $r = $entityManager->getRepository('IssuesBundle\Entity\Issue')
+            ->createQueryBuilder('i')
+            ->select('count(i.id) as c')
+            ->orderBy('i.id', 'desc')
+            ->setMaxResults(1)
+            ->where('i.createdAt >= :today')
+            ->setParameter('today', $today)
+            ->getQuery()
+            ->getSingleResult();
+
+        $id = isset($r['c']) ? (int)$r['c'] + 1: 1;
+
+        $issue->setCode($today->format('Y/m/d').'/'.$id);
+    }
+}
